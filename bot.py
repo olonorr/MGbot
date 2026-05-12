d409fea11255f942f74308a2074bf8efe17d3a00
olonorr <trofimmatveev00@gmail.com>
2026-05-12 17:18:53 +0900

    first commit

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import telebot
 import requests
 from telebot import apihelper
 
-# tg://proxy?server=127.0.0.1&port=1443&secret=dd937831d99b1c474e73fc755198464909
-
 apihelper.proxy = {'https': 'http://127.0.0.1:1443'}
 
 
