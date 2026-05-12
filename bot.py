a9165245c8876a6edd8f69107d8c627659600350
olonorr <trofimmatveev00@gmail.com>
2026-05-12 18:12:08 +0900

    6

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
-#import websockets
+import websockets
 import json
 import asyncio
 
 bot = telebot.TeleBot("8538742738:AAF2QqkbRkMueE1fOg-n7Yb1EFRRnXOjPV4")
 uri = "wss://magicgarden.gg/version/311/api/rooms/7TWG/connect?surface=%22web%22&platform=%22desktop%22&playerId=%22p_KWTb7ix7rFYy9yhS%22&version=%22311%22&anonymousUserStyle=%7B%22color%22%3A%22White%22%2C%22avatarBottom%22%3A%22Bottom_DefaultGray.png%22%2C%22avatarMid%22%3A%22Mid_DefaultGray.png%22%2C%22avatarTop%22%3A%22Top_DefaultGray.png%22%2C%22avatarExpression%22%3A%22Expression_Default.png%22%2C%22name%22%3A%22Sunny+Apple%22%7D&source=%22manualUrl%22&capabilities=%22fbo_mipmap_unsupported%22"
-'''
+
 async def listen():
     async with websockets.connect(uri) as websocket:
         while True:
@@ -28,7 +28,7 @@ async def listen():
 
 def getData() -> str:
     return (asyncio.run(listen()))
-'''
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
