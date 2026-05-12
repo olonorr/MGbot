6035572640180684cba9b95cbb061aa63b91a54c
olonorr <trofimmatveev00@gmail.com>
2026-05-12 17:25:20 +0900

    3

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot(8538742738:AAF2QqkbRkMueE1fOg-n7Yb1EFRRnXOjPV4)
+bot = telebot.TeleBot("8538742738:AAF2QqkbRkMueE1fOg-n7Yb1EFRRnXOjPV4")
 
 
 @bot.message_handler(commands=['start'])
