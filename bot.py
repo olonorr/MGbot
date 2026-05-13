43adf491ebe1189dbc47c4da0d44b8b6f258ceda
olonorr <trofimmatveev00@gmail.com>
2026-05-13 23:26:32 +0900

    21

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ async def listen_and_notify(context: ContextTypes.DEFAULT_TYPE):
     versions_to_try = ["312", "313", "314", "315"]
     
     for version in versions_to_try:
-        uri = f"wss://magicgarden.gg/version/{version}/api/rooms/{ROOM_CODE}/connect?surface=%22web%22&platform=%22desktop%22&playerId=%22{PLAYER_ID}%22&version=%22{version}%22&anonymousUserStyle=%7B%22color%22%3A%22White%22%2C%22avatarBottom%22%3A%22Bottom_DefaultGray.png%22%2C%22avatarMid%22%3A%22Mid_DefaultGray.png%22%2C%22avatarTop%22%3A%22Top_DefaultGray.png%22%2C%22avatarExpression%22%3A%22Expression_Default.png%22%2C%22name%22%3A%22Sunny+Apple%22%7D&source=%22manualUrl%22&capabilities=%22fbo_mipmap_unsupported%22"
+        uri = f"wss://magicgarden.gg/version/{version}/api/rooms/7TWG/connect?surface=%22web%22&platform=%22desktop%22&playerId=%22p_KWTb7ix7rFYy9yhS%22&version=%22{version}%22&anonymousUserStyle=%7B%22color%22%3A%22White%22%2C%22avatarBottom%22%3A%22Bottom_DefaultGray.png%22%2C%22avatarMid%22%3A%22Mid_DefaultGray.png%22%2C%22avatarTop%22%3A%22Top_DefaultGray.png%22%2C%22avatarExpression%22%3A%22Expression_Default.png%22%2C%22name%22%3A%22Sunny+Apple%22%7D&source=%22manualUrl%22&capabilities=%22fbo_mipmap_unsupported%22"
         
         try:
             async with websockets.connect(uri, close_timeout=3) as websocket:
